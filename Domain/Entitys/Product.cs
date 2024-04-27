199bab238d454b46b3bf329a19ef913c1f58d208
Bruno M <c.brunomelo@outlook.com>
2024-04-27 10:14:25 -0300

    Adicionado entidades ao dominio

---
+++ Domain/Entitys/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entitys
+{
+    internal class Product : Entity
+    {
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+        public string Description { get; private set; }
+        public ProductCategory Category { get; private set; }
+
+        internal Product(string name, decimal price, string description)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+        }
+
+        internal void SetCategory(ProductCategory category)
+        {
+            Category = category;
+        }
+
+    }
+}
