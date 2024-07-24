a1d099c0ae6a16dcd85520d9db563d058a4a1fe7
Bruno M <c.brunomelo@outlook.com>
2024-07-23 22:02:58 -0300

    criado handler para o estoque e adicionado fluxo para entrada de estoque

---
+++ Domain/Entitys/Stock.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Observers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entitys
+{
+    public class Stock : Entity
+    {
+        public string Name { get; private set; }       
+        public decimal Quantity { get; private set; }
+        public decimal AvaragePrice { get; private set; }
+        public decimal TotalValue { get; private set; }
+        public Product Product { get; private set; }
+        private List<Client> _clientObservers { get; set; }
+        public IReadOnlyCollection<Client> ClientObservers => _clientObservers;
+        public void setEntry(decimal quantity, decimal totalValue)
+        {
+            Quantity += quantity;
+            TotalValue += totalValue;
+            AvaragePrice = TotalValue / Quantity;
+        }
+
+        public void AttachObserver(Client client)
+        {
+            _clientObservers.Add(client);
+        }
+
+        public void DetachObserver(Client client)
+        {
+            _clientObservers.Remove(client);
+        }
+    }
+}
---
+++ Domain/Repository/IStockRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entitys;
+
+namespace Domain.Repository
+{
+    public interface IStockRepository
+    {
+        void DecreaseStock(int productId, int quantity);
+        Stock Get(int stockId);
+    }
+}
