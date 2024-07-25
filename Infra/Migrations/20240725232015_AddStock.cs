b627c3023377cc32c439179bc1a2022db8d0f5b8
Bruno M <c.brunomelo@outlook.com>
2024-07-25 20:20:30 -0300

    adicionado migrations para stock

---
+++ Infra/Migrations/20240725232015_AddStock.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStock : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StockId",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StockId",
+                table: "Clients",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Stock",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Quantity = table.Column<decimal>(type: "TEXT", nullable: false),
+                    AvaragePrice = table.Column<decimal>(type: "TEXT", nullable: false),
+                    TotalValue = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stock", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_StockId",
+                table: "Products",
+                column: "StockId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_StockId",
+                table: "Clients",
+                column: "StockId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clients_Stock_StockId",
+                table: "Clients",
+                column: "StockId",
+                principalTable: "Stock",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Stock_StockId",
+                table: "Products",
+                column: "StockId",
+                principalTable: "Stock",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clients_Stock_StockId",
+                table: "Clients");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Stock_StockId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Stock");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_StockId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Clients_StockId",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "StockId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "StockId",
+                table: "Clients");
+        }
+    }
+}
