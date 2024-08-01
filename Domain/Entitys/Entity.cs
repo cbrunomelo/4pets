b7c6ba5fc9490ad0e1789dde53cdefc0bd7c62f0
Bruno M <c.brunomelo@outlook.com>
2024-08-01 19:52:31 -0300

    criado status para todas as entididade, podendo ser ativo ou inativo.

--- Domain/Entitys/Entity.cs
+++ Domain/Entitys/Entity.cs
@@ -1,7 +1,10 @@
-﻿namespace Domain.Entitys
+﻿using Domain.Entitys.Enuns;
+
+namespace Domain.Entitys
 {
     public abstract class Entity
     {
         public int Id { get; protected set; }
+        public EStatus Status { get; protected set; }
     }
 }
