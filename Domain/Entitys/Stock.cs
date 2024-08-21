4f1dd80c1a32256de6cde954b67ee866a1316ce4
Bruno Melo <c.brunomelo@outlook.com>
2024-08-21 17:42:27 -0300

    implementado stock repository

--- Domain/Entitys/Stock.cs
+++ Domain/Entitys/Stock.cs
@@ -14,6 +14,13 @@ namespace Domain.Entitys
         public decimal AvaragePrice { get; private set; }
         public decimal TotalValue { get; private set; }
         public Product Product { get; private set; }
+        public int ProductId {
+            get {
+                if (Product is null)
+                    return 0;
+                return Product.Id;
+            }
+            set { } }
         private List<Client> _clientObservers { get; set; }
         public IReadOnlyCollection<Client> ClientObservers => _clientObservers;
         public void setEntry(decimal quantity, decimal totalValue)
@@ -44,6 +51,11 @@ namespace Domain.Entitys
         }       
             
         
+        public void DecreaseStock(decimal quantity)
+        {
+            Quantity -= quantity;
+            TotalValue -= quantity * AvaragePrice;
+        }
 
     }
 }
---
+++ Infra/Repositorys/StockRepository.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Entitys;
+using Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repositorys
+{
+    public class StockRepository : IStockRepository
+    {
+        private readonly StoreDbContext _context;
+        public StockRepository()
+        {
+            _context = new StoreDbContext();
+        }
+        public void DecreaseStock(int productId, int quantity)
+        {
+            try
+            { 
+                var stock = _context.Stocks.FirstOrDefault(x => x.ProductId == productId);
+                stock?.DecreaseStock(quantity);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        public Stock Get(int stockId)
+        {
+            try
+            {
+                return _context.Stocks.FirstOrDefault(x => x.Id == stockId);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
