9f0dfe8d395c443a90bac3eca8354c1cef0a1f4f
Bruno M <c.brunomelo@outlook.com>
2024-08-22 20:41:54 -0300

    Adicionado result para respostas de serviço da aplicação

---
+++ Application/Messaging/IResultService.cs
@@ -0,0 +1,18 @@
+﻿using Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Messaging
+{
+    internal interface IResultService
+    {
+        IDto Result { get; }
+        bool Sucess { get; }
+        string Message { get; }
+        List<string> Errors { get; }
+
+    }
+}
---
+++ Application/Services/OrderService.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Commands.OrderCommands;
+using Domain.Handlers.Contracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class OrderService
+    {
+        private readonly IHandler<CreateOrderCommand> _orderHandler;
+        public OrderService()
+        {
+
+            _orderHandler = new OrderHandler(
+        }
+    }
+}
