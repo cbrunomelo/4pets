30619d820d61bc63bcedc36822ba6126beca507f
Bruno M <c.brunomelo@outlook.com>
2025-04-24 19:49:36 -0300

    fix testes

--- Test/Domain/Handlers/ProductHandlerTest.cs
+++ Test/Domain/Handlers/ProductHandlerTest.cs
@@ -3,6 +3,7 @@ using Domain.Commands.HistoryCommands;
 using Domain.Entitys;
 using Domain.Handlers;
 using Domain.Handlers.Contracts;
+using Domain.Queries.CategoryQuerys;
 using Domain.Repository;
 using MediatR;
 using Moq;
@@ -24,6 +25,7 @@ namespace Test.Domain.Handlers
             _productRepository = new Mock<IProductRepository>();
             _productRepository.Setup(x => x.Create(It.IsAny<Product>())).Returns(1);
             _mediator = new Mock<IMediator>();
+            _mediator.Setup(x => x.Send(It.IsAny<VerifyCategoryExist>(), It.IsAny<CancellationToken>())).ReturnsAsync(true);
 
         }
 
@@ -35,7 +37,8 @@ namespace Test.Domain.Handlers
             var handler = new ProductHandler(_productRepository.Object, _mediator.Object);
 
             // Act
-            var result =await handler.Handle(command);
+            var ctn = new CancellationToken();
+            var result =await handler.Handle(command, ctn);
 
             // Assert
             Assert.True(result.Sucess);
@@ -51,7 +54,8 @@ namespace Test.Domain.Handlers
             var handler = new ProductHandler(_productRepository.Object, _mediator.Object);
 
             // Act
-            var result =await handler.Handle(command);
+            var ctn = new CancellationToken();
+            var result =await handler.Handle(command, ctn);
 
             // Assert
             Assert.False(result.Sucess);
@@ -68,7 +72,8 @@ namespace Test.Domain.Handlers
             var handler = new ProductHandler(_productRepository.Object, _mediator.Object);
 
             // Act
-            var result = await handler.Handle(command);
+            var ctn = new CancellationToken();
+            var result = await handler.Handle(command, ctn);
 
             // Assert
             Assert.False(result.Sucess);
@@ -85,12 +90,13 @@ namespace Test.Domain.Handlers
             var handler = new ProductHandler(_productRepository.Object, _mediator.Object);
 
             // Act
-            var result = await handler.Handle(command);
+            var ctn = new CancellationToken();
+            var result = await handler.Handle(command, ctn);
 
             // Assert
             Assert.False(result.Sucess);
             Assert.True(result.Errors.Count > 0);
-            _productRepository.Verify(x => x.Create(It.IsAny<Product>()), Times.Once);
+            _productRepository.Verify(x => x.Create(It.IsAny<Product>()), Times.Never);
         }
 
     }
